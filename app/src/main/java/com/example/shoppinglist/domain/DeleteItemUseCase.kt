de6db12b729e179b8217b4be902627adf0559622
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-05-28 15:33:05 +0300

    domain

---
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.domain
+
+class DeleteItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
+
+    }
+}
