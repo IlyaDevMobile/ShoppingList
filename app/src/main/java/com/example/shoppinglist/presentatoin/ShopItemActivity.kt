dc0284ed132dbc49d4b80157ba7ba41ee6c46982
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-06-16 22:05:05 +0300

    fragment(part5)

--- app/src/main/java/com/example/shoppinglist/presentatoin/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentatoin/ShopItemActivity.kt
@@ -25,6 +25,9 @@ class ShopItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
         parseIntent()
+        if (savedInstanceState == null){
+            launchRightMode()
+        }
 //        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
 //        initViews()
 //        addTextChangeListeners()
@@ -118,7 +121,7 @@ class ShopItemActivity : AppCompatActivity() {
             else -> throw RuntimeException("Unknown screen mode $screenMode")
         }
         supportFragmentManager.beginTransaction()
-            .add(R.id.shop_item_container, fragment)
+            .replace(R.id.shop_item_container, fragment)
             .commit()
     }
 
